bd7f694c43e17a02408640f17fb781e0313c4502
SviatoslavLevi9 <s.yurkiv@levi9.com>
2022-12-02 18:45:34 +0200

    Update PART4.cy.ts

--- cypress/e2e/PART4.cy.ts
+++ cypress/e2e/PART4.cy.ts
@@ -143,7 +143,7 @@ describe('Browser actions', () => {
         cy.get('div').find('[role="row"]').should('have.length', 31)
     })
             // You should run test above first
-    it.only('Trigger', ()=>{
+    it('Trigger', ()=>{
         cy.visit('https://dpg-ads-stage.selfservice-advertising.dpgmedia.cloud/advertisers/07311119-279f-4dfc-8b63-496d6fada798/performance-dashboard/844')
         cy.wait(1000)
         cy.get('g').find('circle[r="4"]').eq(1).trigger('mouseover').pause()
