461223b908f4c637de484fb2b22a7586e05ed131
SviatoslavLevi9 <s.yurkiv@levi9.com>
2022-12-07 15:02:41 +0200

    Part 5

---
+++ cypress/e2e/PART5.js
@@ -0,0 +1,84 @@
+
+//Task 1
+let country = 'Ukraine';
+let continent = 'Europe';
+let population = 42000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Task 2
+ let isIsland = false;
+ let language;
+
+ console.log(typeof country);
+ console.log(typeof continent);
+ console.log(typeof population);
+ console.log(typeof isIsland);
+ console.log(typeof language);
+
+ //Task 3
+ let changeMe = true;
+ changeMe = false;
+ console.log(changeMe);
+
+ // Task 4
+const entree = 'Enchiladas';
+//entree = 'Hello';
+console.log(entree)
+
+//Task 5
+let levelUp = 10;
+levelUp += 5;
+console.log(levelUp); 
+
+let powerLevel = 9001; 
+powerLevel -= 100;
+console.log(powerLevel)
+
+let multiplyMe = 32; 
+multiplyMe *= 11;
+console.log(multiplyMe);
+
+let quarterMe = 1152;
+quarterMe /= 4;
+console.log(quarterMe);   
+
+//Task 6
+
+let gainedDollar = 3; 
+gainedDollar++;
+console.log(gainedDollar);
+
+let lostDollar = 50;
+lostDollar--;
+console.log(lostDollar) 
+
+let populationOfUkraine = 43810000;
+populationOfUkraine/=2;
+console.log(populationOfUkraine);
+
+populationOfUkraine++;
+console.log(populationOfUkraine);
+
+let finlandPopulation = 6000000;
+let summ = populationOfUkraine > finlandPopulation;
+//console.log(populationOfUkraine > finlandPopulation);
+console.log(summ);
+
+let contryPopulation = 33000000;
+let summ2 = populationOfUkraine < contryPopulation;
+console.log(summ2);
+
+//Task 7
+ let myPet = 'Bony';
+ console.log('I own a pet ' + myPet + '.')
+
+ //Task 8
+  let mySecondPet = 'Tuzik';
+ console.log(`I own a pet ${mySecondPet}.`);
+
+ //Task 9
+ let myName = 'Sviat';
+ let myCity = 'Lviv';
+ console.log('My name is ' + myName +'.' + ' My favorite city is ' + myCity + '.');
