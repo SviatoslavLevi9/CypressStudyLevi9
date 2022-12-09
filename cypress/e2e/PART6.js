aec9c4d6572b0616f77e7d7a0d2ab76a50e5e8b7
SviatoslavLevi9 <s.yurkiv@levi9.com>
2022-12-09 18:08:21 +0200

    Create PART6.js

---
+++ cypress/e2e/PART6.js
@@ -0,0 +1,36 @@
+let time = 9
+
+if (time < 10) {
+    greeting = "Good morning";
+    console.log(greeting);
+} else if (time < 20) {
+    greeting = "Good day";
+    console.log(greeting);
+} else {
+    greeting = "Good evening";
+    console.log(greeting);
+}
+
+let time1 = 11;
+let greeting1;
+if (time1 < 10) {
+    greeting1 = "Good morning";
+
+} else if (time1 < 20) {
+    greeting1 = "Good day";
+
+} else {
+    greeting1 = "Good evening";
+}
+console.log(greeting1);
+
+
+
+// Task 1
+let sale = false;
+
+if (sale == true) {
+    console.log('Time to buy!')
+} else {
+    console.log('Time to wait for a sale.')
+}
