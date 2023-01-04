a8392c461d35953d673a9b89d89312ca0349acfa
SviatoslavLevi9 <s.yurkiv@levi9.com>
2023-01-04 12:40:03 +0200

    Update regression.cy.ts

--- cypress/e2e/regression.cy.ts
+++ cypress/e2e/regression.cy.ts
@@ -62,6 +62,36 @@ describe('Browser actions', () => {
         cy.get('div[role="radiogroup"]').find('[name="gender"]').eq(1).check()
         cy.get('div').find('input[name="age"]').eq(1).check()
         cy.get('div').find('[data-testid="datePicker-endDate"]').click()
+        cy.get('.DayPicker-Month', {timeout: 15000}).should('be.visible')
+        cy.get('div').find('[aria-selected="true"]').click()
+        cy.get('div').find('[name="budgetType"]').eq(1).check()
+        cy.get('button[type="submit"]').click()
+        cy.wait(1000)
+        cy.get('button[type="submit"]').click()
+        cy.get('form').find('h2').eq(0).should('have.text', 'Inloggen')
+        cy.get('form').find('h2').eq(1).should('have.text', 'Nieuw bij Direct?')
+        // You should change your email and user name because with the existing email, this test will fall
+        cy.get('input[name="name"]').eq(0).type('NewRegressionUser1')
+        cy.get('input[name="email"]').eq(1).type('NRU1@yopmail.com')
+        
+        cy.get('button[type="submit"]').eq(1).should('contain','Creeër een account').click()
+
+
+        cy.get('input[name="companyName"]').type('Levi9')
+        cy.get('input[name="kvk"]').type('34224746')
+        cy.get('input[name="phoneNumber"]').type('2345666484290')
+        cy.get('input[name="password"]').type('Sava1992!')
+        cy.get('input[name="paymentMethod"]').eq(0).check()
+        cy.get('div').find('[aria-haspopup="listbox"]').click()
+        cy.get('ul').find('[data-value="ideal_REVOLT21"]').click()
+        cy.get('input[name="conditions"]').check()
+        cy.get('button[type="submit"]').should('contain','Creeër een account').click()
+        cy.wait(50000)
+
+
+
+
+
 
 
 
